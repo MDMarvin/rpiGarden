ed3c48d675b54a84ec8cb47974e52d89e2efa71c
Marvin Grapatin <i.gepatin@gmail.com>
2019-03-14 13:24:40 +0100

    Fix RestController Annotation

--- src/main/java/de/grapatin/controller/RpiController.java
+++ src/main/java/de/grapatin/controller/RpiController.java
@@ -2,7 +2,9 @@ package de.grapatin.controller;
 
 import com.pi4j.io.gpio.*;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class RpiController {
 
     private static GpioPinDigitalOutput pin;
