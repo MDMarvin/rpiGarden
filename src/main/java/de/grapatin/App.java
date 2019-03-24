26c4055072cd994c782c0ccf23d3978eccb4ccf7
mgrapatin <marvin.grapatin@md.de>
2019-03-24 12:06:41 +0100

    Application now gives a shutdown.pid file at start

--- src/main/java/de/grapatin/App.java
+++ src/main/java/de/grapatin/App.java
@@ -1,12 +1,15 @@
 package de.grapatin;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplicationBuilder applicationBuilder = new SpringApplicationBuilder(App.class);
+        applicationBuilder.build().addListeners(new ApplicationPidFileWriter("./bin/shutdown.pid"));
+        applicationBuilder.run(args);
     }
 }
